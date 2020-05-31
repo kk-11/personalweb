cb5628736c9e44ad6fad0cc46a49708deb36e800
killian <killian.debuitleir@gmail.com>
2020-05-31 21:12:45 +0100

    sass and router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './components/App.js';
+import Home from './components/home/home.js';
+import About from './components/about/about.js';
+import Error from './components/error/error.js';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './styles.sass';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+function fireGA() {
+	console.log('here', window.gtag);
+	window.gtag('event', 'xyz');
+}
+export default function App () {
+	return (
+		<main>
+			<Switch>
+				<Route path="/" component={Home} exact />
+				<Route path="/about" component={About} />
+				<Route component={Error} />
+			</Switch>
+		</main>
+	);
+}
+
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>, document.getElementById('app')
+);
---
+++ src/notes/boilerplate-setup.js
@@ -0,0 +1,10 @@
+export const text = `
+	31/5/2020 - Boiler plate set up
+	---
+	This is something that I've wanted to do for a while. To set up a minimal react and webpack boilerplate.
+	The goal was to learn a bit more about webpack rather than relying on create-react-app. And to know what each
+	package I'm installing is actually used for.
+	While I'm still far from an expert, it was pretty interesting getting everything to work.
+	There was quite a bit of trial and error trying to convert my old set up over from create-react-app so ultimately I just started from scratch
+	and followed along with a few tutorials.
+`;
