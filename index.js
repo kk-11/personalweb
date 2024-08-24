b7a29f0947a85e5aac0d6cfda1f840a360674f9b
killian <killian.debuitleir@gmail.com>
2024-08-24 14:09:08 +0200

    clean up

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const canvas = document.getElementById('canvas'),
-  context = canvas.getContext('2d'),
-  width = (canvas.width = window.innerWidth),
-  height = (canvas.height = window.innerHeight);
-mouse = { x: 0, y: 0 };
+const canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = (canvas.width = window.innerWidth),
+    height = (canvas.height = window.innerHeight);
+
+let scalar = 0;
+let mouse = { x: 0, y: 0 };
+
 function mousemove(event) {
-  mouse = { x: event.clientX, y: event.clientY };
+    mouse = { x: event.clientX, y: event.clientY };
 }
-window.addEventListener('mousemove', mousemove);
+window.addEventListener("mousemove", mousemove);
 context.lineWidth = 0.1;
-this.mouse = { x: 0, y: 0 };
 
 const a = Math.random() * 10 - 2;
 const b = Math.random() * 4 - 2;
@@ -17,50 +19,60 @@ const d = Math.random() * 4 - 2;
 
 const points = [];
 for (let y = 0; y < height; y += 20) {
-  points.push({
-    x: 0,
-    y: y,
-    vx: 0,
-    vy: 0,
-  });
+    points.push({
+        x: 0,
+        y: y,
+        vx: 0,
+        vy: 0,
+    });
 }
 
 render();
 
 function render() {
-  for (let i = 0; i < points.length; i++) {
-    const p = points[i];
-    const value = getValue(2 * p.x - mouse.x, 2 * p.y - mouse.y);
-    p.vx += Math.cos(value) * 0.1;
-    p.vy += Math.sin(value) * 0.1;
-    context.beginPath();
-    context.moveTo(p.x, p.y);
-    p.x += p.vx;
-    p.y += p.vy;
-    context.lineTo(p.x, p.y);
+    if (scalar > 1) {
+        scalar *= 0.9;
+    }
+    if (scalar < 1) {
+        scalar = 1;
+    }
+    for (let i = 0; i < points.length; i++) {
+        const p = points[i];
+        const value = getNextValue(2 * p.x - mouse.x, 2 * p.y - mouse.y);
+        p.vx += Math.cos(value) * 0.2;
+        p.vy += Math.sin(value) * 0.2;
+        context.beginPath();
+        context.moveTo(p.x, p.y);
+        p.x += p.vx;
+        p.y += p.vy;
+        context.lineTo(p.x, p.y);
 
-    context.stroke();
-    p.vx *= 0.99;
-    p.vy *= 0.99;
-    context.closePath();
-  }
+        context.stroke();
+        p.vx *= 0.99;
+        p.vy *= 0.99;
+        context.closePath();
+    }
 
-  requestAnimationFrame(render);
+    requestAnimationFrame(render);
 }
 
-function getValue(x, y) {
-  // http://paulbourke.net/fractals/clifford/
-  const scale = 0.005;
-  x = (x - width / 2) * scale;
-  y = (y - height / 2) * scale;
+function getNextValue(x, y) {
+    // http://paulbourke.net/fractals/clifford/
+    const scale = 0.005;
+    x = (x - width / 2) * scale;
+    y = (y - height / 2) * scale;
 
-  const x1 = Math.sin(a) - Math.cos(b * x);
-  const y1 = Math.sin(c * x) - Math.cos(d * y);
+    const x1 = Math.sin(a) - Math.cos(b * x) * scalar;
+    const y1 = Math.sin(c * x) - Math.cos(d * y) * -scalar;
 
-  return Math.atan2(y1 - y, x1 - x);
+    return Math.atan2(y1 - y, x1 - x) * scalar;
 }
 
-const showmore = () => {
-  document.getElementById('showmore').classList.toggle('active');
-  document.getElementById('moreinfo').classList.toggle('active');
-};
+function spread() {
+    scalar = 100;
+}
+
+function showmore() {
+    document.getElementById("showmore").classList.toggle("active");
+    document.getElementById("moreinfo").classList.toggle("active");
+}
